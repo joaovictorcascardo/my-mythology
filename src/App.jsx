f344f4b4a73a5c91a5420b90349e3308ec8d6c27
KaioBosloni <kaiobolsonioriginal@gmail.com>
2026-03-29 20:12:30 -0300

    feat: implementa componente SearchBar para pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { criaturas } from "./data/criaturas";
 import CardCriatura from "./components/CardCriatura";
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  return (
+
+  const [busca, setBusca] = useState("");
+  const criaturasFiltradas = criaturas.filter(criatura =>
+    criatura.nome.toLowerCase().includes(busca.toLowerCase())
+  );
+
+return (
     <div className="mx-auto max-w-7xl p-8 text-center">
-      <h1 className="mb-8 text-4xl font-bold text-purple-500">
+      <h1 className="mb-9 text-4xl font-bold text-red-600">
         🏛️ My Mythology
       </h1>
+      <SearchBar valor={busca} aoDigitar={setBusca} />
+      
       <div className="mx-auto grid max-w-5xl gap-6 text-left sm:grid-cols-2 lg:grid-cols-3">
-        {criaturas.map(({ id, ...props }) => (
-        <CardCriatura key={id} {...props} />
+        {criaturasFiltradas.map(({ id, ...props }) => (
+          <CardCriatura key={id} {...props} />
         ))}
       </div>
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,13 @@
-export default function SearchBar() {
+export default function SearchBar({ valor, aoDigitar }) {
   return (
-    <div className="search-bar">
-      <p>Barra de Busca</p>
+    <div className="mb-8">
+      <input
+        type="text"
+        placeholder="PESQUISAR SUA CRIATURA"
+        value={valor}
+        onChange={(e) => aoDigitar(e.target.value)}
+        className="w-full max-w-md p-2 border border-red-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
     </div>
   );
 }
