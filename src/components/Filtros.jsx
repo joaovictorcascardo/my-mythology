f1ed09dc1453ffee187c779a0a75ef7d8e2ad57d
HeitorPolastri <polastrit@gmail.com>
2026-04-09 21:41:08 -0300

    feat: cria componente de select para o filtro de categorias

--- src/components/Filtros.jsx
+++ src/components/Filtros.jsx
@@ -1,7 +1,17 @@
-export default function Filtros() {
+export default function Filtros({ categoriaSelecionada, aoMudarCategoria }) {
   return (
-    <div className="filtros">
-      <p>Filtros de Categoria</p>
+    <div className="mb-8">
+      <select
+        value={categoriaSelecionada}
+        onChange={(e) => aoMudarCategoria(e.target.value)}
+        className="w-full max-w-md p-2 border border-red-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+      >
+        <option value="">Todas as Mitologias</option>
+        <option value="Mitologia Grega">Mitologia Grega</option>
+        <option value="Mitologia Nórdica">Mitologia Nórdica</option>
+        <option value="Mitologia Egípcia">Mitologia Egípcia</option>
+        <option value="Mitologia Japonesa">Mitologia Japonesa</option>
+      </select>
     </div>
   );
 }
