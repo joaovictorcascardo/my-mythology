0185f914b40e293b2c720801ee84c41f66936e09
Joao Prado <Joaovictorpachecoleite@gmail.com>
2026-04-09 22:16:19 -0300

    feat: integra componente de Filtros e implementa logica de filtro duplo por nome e categoria

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import { criaturas } from "./data/criaturas";
 import CardCriatura from "./components/CardCriatura";
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import Filtros from "./components/Filtros";
 
 function App() {
-
   const [busca, setBusca] = useState("");
-  const criaturasFiltradas = criaturas.filter(criatura =>
-    criatura.nome.toLowerCase().includes(busca.toLowerCase())
-  );
+  const [categoria, setCategoria] = useState("");
 
-return (
+  const criaturasFiltradas = criaturas.filter((criatura) => {
+    const nomeBate = criatura.nome.toLowerCase().includes(busca.toLowerCase());
+    const categoriaBate = categoria === "" || criatura.categoria === categoria;
+    return nomeBate && categoriaBate;
+  });
+
+  return (
     <div className="mx-auto max-w-7xl p-8 text-center">
-      <h1 className="mb-9 text-4xl font-bold text-red-600">
-        🏛️ My Mythology
-      </h1>
+      <h1 className="mb-9 text-4xl font-bold text-red-600">🏛️ My Mythology</h1>
       <SearchBar valor={busca} aoDigitar={setBusca} />
-      
+
+      <Filtros
+        categoriaSelecionada={categoria}
+        aoMudarCategoria={setCategoria}
+      />
+
       <div className="mx-auto grid max-w-5xl gap-6 text-left sm:grid-cols-2 lg:grid-cols-3">
         {criaturasFiltradas.map(({ id, ...props }) => (
           <CardCriatura key={id} {...props} />
